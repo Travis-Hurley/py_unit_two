79bb721dc407382191bfc06f956a62b9fe80ab7f
Travis-Hurley <hurlean@ssfs.org>
2022-09-20 17:16:55 -0400

    Completed 2.3

--- operators.py
+++ operators.py
@@ -1,3 +1,15 @@
-print(2 + 3)
-print(2 - 3)
-print(2 * 3)
+print(2 + 3) #adds
+print(2 - 3) #subtraction
+print(2 * 3) #multiplacation
+print(2 ** 3) #to the power
+print(3 ** 2) #power
+print(9 * 5) #multplacation
+print(2 / 5) #division
+print(12 // 5) #divides and rounds
+print(27 // 4) #divides and rounds
+print(2 // 5) #divides and rounds
+print(5 % 2) #remainder
+print(9 % 5)#remainder
+print(6 % 6)#remainder
+print(2 % 7)#remainder
+
