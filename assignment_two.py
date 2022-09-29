d11d2b6609ba6affb0ecff3955ce0d6915f3aabc
Travis-Hurley <hurlean@ssfs.org>
2022-09-29 10:53:42 -0400

    ok ok fine its really done this time

--- assignment_two.py
+++ assignment_two.py
@@ -1,8 +1,13 @@
 import math
 import turtle
-print("Enter two different 'x' and 'y' points and I will show you the angle they make!")
+print("Enter two different 'x' and 'y' points and I will show you the acute angle they make!")
 understand=input('Cool?')
-print("Glad we are on the same page.")
+if "no" in understand:
+    print("go away then, jacob")
+if "cool" in understand:
+    print("Alright! Lets get started.")
+else:
+    print("Glad we are on the same page.")
 value1=input('What is the first x coordinate')
 value2=input('What is the first y coordinate')
 value3=input('What is the second x coordinate')
@@ -12,13 +17,16 @@ y1=int(value2)
 x2=int(value3)
 y2=int(value4)
 slope1=(y2-y1)/(x2-x1)
-slope2=(y1)/(x1+1)
+if (x1)==0:
+    x1=(x1+1)
+slope2=(y2)/(x1)
+
 if slope1>slope2:
     angle = math.atan((slope1 - slope2) / 1 + (slope1 * slope2))
 if slope2>slope1:
     angle = math.atan((slope2-slope1) / 1 + (slope1 * slope2))
-angle_degrees= angle*180/math.pi
-round(angle_degrees)
+angle_degrees= round(angle*180/math.pi)
+
 print('Your angle is',abs(angle_degrees))
 
 turtle.goto(x1,y1)
